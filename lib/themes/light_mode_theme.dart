639935ac3467daa5b99332365a117a09b488257f
abdul Razzaq <ar8937562@gmail.com>
2023-11-28 08:15:01 -0800

    created light mode class for manage the colors and themes of project

---
+++ lib/themes/light_mode_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData LightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade400,
+    primary: Colors.grey.shade200,
+    secondary: Colors.orange.shade600,
+    inversePrimary: Colors.grey.shade900
+  )
+);
