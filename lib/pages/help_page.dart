fae65aa76a2c06de413545bcd92e361308640be0
abdul Razzaq <ar8937562@gmail.com>
2023-11-28 08:13:47 -0800

    created another page the help page where we can help customers through ordering or buying the products. also solving their queries

--- lib/pages/help_page.dart
+++ lib/pages/help_page.dart
@@ -13,15 +13,91 @@ class HelpPage extends StatelessWidget {
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(15.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Card(
-              color: Colors.white,
-              child: Text("Please Help me in getting right choice?",style: TextStyle(),),
-            )
+          children: [
+            AspectRatio(
+              aspectRatio: 3,
+              child: ExpansionTile(
+                title: const Text(
+                  "Please Help me in getting right choice?",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                children: [
+                  Container(
+                    child: const Text(
+                      "Our all Products are high quality and Premuim and "
+                      "don't hesitate in buying products because we "
+                      "offer emotions rather than items.",
+                      style: TextStyle(fontSize: 15, color: Colors.black87),
+                    ),
+                    // color: Colors.black87,
+                  ),
+                ],
+              ),
+            ),
+            const AspectRatio(
+              aspectRatio: 3,
+              child: ExpansionTile(
+                title: Text(
+                  "Is there any process for return back item?",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                children: [
+                  Text(
+                    "Yes! You can go to TCS or any other service then submit there. Firstly you have to contact us.",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  )
+                ],
+              ),
+            ),
+            const AspectRatio(
+              aspectRatio: 3,
+              child: ExpansionTile(
+                title: Text(
+                  "How much warranty will you give?",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                children: [
+                  Text(
+                    "Firstly, we don't give you warranty because our products never betray customers and our Products are Priceless.",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                ],
+              ),
+            ),
+            const AspectRatio(
+              aspectRatio: 3,
+              child: ExpansionTile(
+                title: Text(
+                  "How many days the item will be delivered?",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                children: [
+                  Text(
+                    "We also don't want to delay in our delivery. Normal delivery days are monday to Friday.",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                ],
+              ),
+            ),
+            const AspectRatio(
+              aspectRatio: 3,
+              child: ExpansionTile(
+                title: Text(
+                  "Other?",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                children: [
+                  Text(
+                    "Contact Us: (0322-2122-121)",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
